dd45d29f45666d243d9e13a5e04cb0e1e6cff858
Sanjana Jain <sanjana.jain04@gmail.com>
2020-10-11 05:14:17 +0530

    Valid first name

---
+++ src/com/cg/user_reg/UC1_Username.java
@@ -0,0 +1,22 @@
+package com.cg.user_reg;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class UC1_Username {
+ public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter your first name: ");
+	String first_name=sc.nextLine();
+	boolean check=true;
+	while(check) {
+	if(Pattern.matches("[A-Z][a-zA-Z]{2,}",first_name))
+		check=false;
+	else {
+		System.out.println("Invalid First Name");
+		System.out.println("Enter your First Name: ");
+	    first_name=sc.nextLine();
+	}	
+   } 
+  }
+ 
+}
